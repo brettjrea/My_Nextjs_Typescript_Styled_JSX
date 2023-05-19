7d1b844b052c6488aa8e912759208ad696efce66
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-05-19 18:29:24 -0400

    Update Main.tsx

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,7 +1,12 @@
-export default function Main({ children }) {
+// components/Main.tsx
+
+export default function Main({ isSidebarOpen }) {
   return (
     <main>
-      {children}
+      {/* Insert the main content of your site here */}
+      <h1>Welcome to Brett J Rea's Portfolio</h1>
+      <p>...</p>
+
       <style jsx>{`
         main {
           margin-left: ${isSidebarOpen ? '250px' : '0'};
