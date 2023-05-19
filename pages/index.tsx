902386d451b021bd485b6bfec617ad586efda24b
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-05-19 18:18:33 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
+import Main from '../components/Main'
 import Link from 'next/link'
 
 export default function IndexPage() {
