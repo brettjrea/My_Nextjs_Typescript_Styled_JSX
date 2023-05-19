17ea990c209147f324e9ac5ba428341557eae257
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-05-19 18:28:21 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+// components/Header.tsx
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -10,6 +11,16 @@ export default function Header({ isDarkMode, toggleTheme, toggleSidebar }) {
       <button onClick={toggleTheme}>
         <FontAwesomeIcon icon={isDarkMode ? faSun : faMoon} />
       </button>
+
+      <style jsx>{`
+        header {
+          display: flex;
+          justify-content: space-between;
+          padding: 1em;
+          background-color: ${isDarkMode ? '#333' : '#fff'};
+          color: ${isDarkMode ? '#fff' : '#333'};
+        }
+      `}</style>
     </header>
   )
 }
