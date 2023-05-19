bb6d638aa30b2cf2a24a172b6e5f165cefdcd545
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-05-19 18:28:49 -0400

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,4 @@
+// components/Sidebar.tsx
 import Link from 'next/link'
 
 export default function Sidebar({ isSidebarOpen }) {
@@ -9,6 +10,18 @@ export default function Sidebar({ isSidebarOpen }) {
         <li><Link href="/about">About</Link></li>
         {/* Add more navigation links here */}
       </ul>
+
+      <style jsx>{`
+        nav {
+          position: fixed;
+          left: 0;
+          top: 0;
+          bottom: 0;
+          width: 250px;
+          background-color: #f5f5f5;
+          overflow: auto;
+        }
+      `}</style>
     </nav>
   )
 }
