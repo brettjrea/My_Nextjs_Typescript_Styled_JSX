49cc7a757320facea5812f94089adcd0f02fbf2c
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-05-19 18:36:07 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,28 @@
+// pages/index.tsx
 import { useState, useEffect } from 'react'
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
 import Main from '../components/Main'
-import Link from 'next/link'
 
 export default function IndexPage() {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  // Retrieve theme preference from local storage when component mounts
-  useEffect(() => {
-    const savedTheme = localStorage.getItem('theme');
-    setIsDarkMode(savedTheme === 'dark');
-  }, []);
-
   const toggleTheme = () => {
-    const newTheme = !isDarkMode;
-    setIsDarkMode(newTheme);
+    setIsDarkMode(!isDarkMode);
+    localStorage.setItem('isDarkMode', JSON.stringify(!isDarkMode));
+  };
 
-    // Save theme preference in local storage
-    localStorage.setItem('theme', newTheme ? 'dark' : 'light');
-  }
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  useEffect(() => {
+    const storedThemePreference = JSON.parse(localStorage.getItem('isDarkMode'));
+    if (storedThemePreference) {
+      setIsDarkMode(storedThemePreference);
+    }
+  }, []);
 
   return (
     <div className={isDarkMode ? 'dark' : 'light'}>
@@ -30,60 +32,15 @@ export default function IndexPage() {
         toggleSidebar={toggleSidebar}
       />
       <Sidebar isSidebarOpen={isSidebarOpen} />
-      <main>
-      <h1>Hi there 👋</h1>
-      <h2>My name is Brett. I love computers, the internet, and learning to program for them.</h2>
-      <ul>
-        <li>🔭 I’m currently working on getting my bachelor's in computer science.</li>
-        <li>🌱 I’m currently taking Written and Verbal Communication for Software Development.</li>
-        <li>👯 I’m looking to collaborate on anything cool and relevant.</li>
-      </ul>
-      <h3>Here are some ideas to get you started:</h3>
-      <ul>
-        <li>🔭 I’m currently working on getting my bachelor's in computer science.</li>
-        <li>🌱 I’m currently learning Data Structures and Algorithms.</li>
-        <li>🤔 I’m looking for help with ...</li>
-        <li>💬 Ask me about ...</li>
-        <li>📫 How to reach me: ...</li>
-        <li>😄 Pronouns: ...</li>
-        <li>⚡ Fun fact: ...</li>
-      </ul>
-      <img src="https://github-readme-stats.vercel.app/api?username=brettjrea&show_icons=true&theme=transparent" alt="Brett J Rea's GitHub stats"/>
-      </main>
+      <Main isSidebarOpen={isSidebarOpen} />
       <style jsx>{`
-        div {
-          margin: 0 auto;
-          max-width: 800px;
-          padding: 1em;
-          font-family: Arial, sans-serif;
-          color: ${isDarkMode ? '#DDD' : '#333'};
-          background-color: ${isDarkMode ? '#333' : '#FFF'};
-          transition: all 0.5s ease;
+        .light {
+          background-color: #fff;
+          color: #333;
         }
-        h1 {
-          color: ${isDarkMode ? '#CCC' : '#0070f3'};
-        }
-        button {
-          padding: 10px;
-          margin-bottom: 20px;
-          background-color: ${isDarkMode ? '#0070f3' : '#333'};
-          color: ${isDarkMode ? '#333' : '#FFF'};
-          border: none;
-          cursor: pointer;
-          transition: all 0.5s ease;
-        }
-        button:hover {
-          background-color: ${isDarkMode ? '#0050b3' : '#111'};
-        }
-        ul {
-          padding-left: 20px;
-        }
-        li {
-          line-height:1.5;
-        }
-        img {
-          max-width: 100%;
-          height: auto;
+        .dark {
+          background-color: #333;
+          color: #fff;
         }
       `}</style>
     </div>
