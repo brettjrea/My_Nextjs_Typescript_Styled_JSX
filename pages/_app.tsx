95644ddf2aed7199e993090e9b0bca0295e252ef
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-05-19 23:33:05 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,3 +6,24 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+// pages/_app.tsx
+import { useState } from 'react'
+import Header from '../components/Header'
+import Main from '../components/Main'
+import Sidebar from '../components/Sidebar'
+
+function MyApp({ Component, pageProps }) {
+  const [isDarkMode, setIsDarkMode] = useState(false)
+  const toggleTheme = () => setIsDarkMode(!isDarkMode)
+
+  return (
+    <div className="app">
+      <Header isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
+      <Sidebar isDarkMode={isDarkMode} />
+      <Main isDarkMode={isDarkMode} />
+      <Component {...pageProps} />
+    </div>
+  )
+}
+
+export default MyApp
