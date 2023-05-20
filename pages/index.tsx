cdacf104647bdb1bed5fb24bd833cc71bf2c51b3
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-05-20 10:46:51 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@
 import { useState, useEffect } from 'react'
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
-import Main from '../components/Main'
 
 export default function IndexPage() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -24,16 +23,42 @@ export default function IndexPage() {
     }
   }, []);
 
-return (
-  <div className={isDarkMode ? 'dark' : 'light'} style={{height: '100vh', display: 'flex', flexDirection: 'column'}}>
-    <div style={{ display: 'flex', flex: '1' }}>
-      <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} isDarkMode={isDarkMode}/>
-      <div style={{width: '100%'}}>
-        <Header isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
-        <Main isDarkMode={isDarkMode}/>
+  return (
+    <div className={isDarkMode ? 'dark' : 'light'} style={{height: '100vh', display: 'flex', flexDirection: 'column'}}>
+      <div style={{ display: 'flex', flex: '1' }}>
+        <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} isDarkMode={isDarkMode}/>
+        <div style={{width: '100%'}}>
+          <Header isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
+          <main>
+            <h1>Hi there 👋</h1>
+            <h2>My name is Brett. I love computers, the internet, and learning to program for them.</h2>
+            <ul>
+              <li>🔭 I’m currently working on getting my bachelor's in computer science.</li>
+              <li>🌱 I’m currently taking Written and Verbal Communication for Software Development.</li>
+              <li>👯 I’m looking to collaborate on anything cool and relevant.</li>
+            </ul>
+            <h3>Here are some ideas to get you started:</h3>
+            <ul>
+              <li>🔭 I’m currently working on getting my bachelor's in computer science.</li>
+              <li>🌱 I’m currently learning Data Structures and Algorithms.</li>
+              <li>🤔 I’m looking for help with ...</li>
+              <li>💬 Ask me about ...</li>
+              <li>📫 How to reach me: ...</li>
+              <li>😄 Pronouns: ...</li>
+              <li>⚡ Fun fact: ...</li>
+            </ul>
+            <img src="https://github-readme-stats.vercel.app/api?username=brettjrea&show_icons=true&theme=transparent" alt="Brett J Rea's GitHub stats" width="100%" height="auto"/>
+
+            <style jsx>{`
+              main {
+                padding: 1em;
+                background: ${isDarkMode ? 'black' : 'white'};
+                color: ${isDarkMode ? 'white' : 'black'};
+              }
+            `}</style>
+          </main>
+        </div>
       </div>
     </div>
-    ...
-  </div>
-)
+  )
 }
