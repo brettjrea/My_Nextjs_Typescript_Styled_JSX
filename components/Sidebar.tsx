3d9ca7e2febb738537aff7e6358bd6284e581b69
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-05-20 00:31:42 +0000

    fix

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,30 +3,26 @@ import Link from 'next/link'
 
 export default function Sidebar({ isSidebarOpen, toggleSidebar }) {
   return (
-    <nav>
-      <button onClick={toggleSidebar} style={{position: 'absolute', left: isSidebarOpen ? '250px' : '0'}}>
+    <div style={{ position: 'relative', width: isSidebarOpen ? '250px' : '50px' }}>
+      <nav>
+        {isSidebarOpen &&
+          <ul>
+            <li><Link href="/about">About</Link></li>
+            {/* Add more navigation links here */}
+          </ul>
+        }
+        <style jsx>{`
+          nav {
+            width: 100%;
+            background-color: #f5f5f5;
+            overflow: auto;
+            transition: width 0.5s ease;
+          }
+        `}</style>
+      </nav>
+      <button onClick={toggleSidebar} style={{ position: 'absolute', right: '-20px', top: '0', backgroundColor: 'red' }}>
         {isSidebarOpen ? '<' : '>'}
       </button>
-      {isSidebarOpen &&
-        <ul>
-          <li><Link href="/about">About</Link></li>
-          {/* Add more navigation links here */}
-        </ul>
-      }
-      <style jsx>{`
-        nav {
-          position: absolute;
-          width: ${isSidebarOpen ? '250px' : '0'};
-          background-color: #f5f5f5;
-          overflow: auto;
-          transition: width 0.5s ease;
-        }
-        button {
-          background: none;
-          border: none;
-          color: #333; // or any other color
-        }
-      `}</style>
-    </nav>
+    </div>
   )
 }
