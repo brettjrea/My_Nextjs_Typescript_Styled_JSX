8a73a0f87f85532bf1550870e719b99f9610e61b
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-05-20 01:11:14 +0000

    fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,13 @@
 // components/Header.tsx
-export default function Header({ isDarkMode, toggleTheme, toggleSidebar, isSidebarOpen }) {
+export default function Header({ isDarkMode, toggleTheme }) {
   return (
     <header>
-      <button onClick={toggleSidebar}>
-          {isSidebarOpen ? '<' : '>'}
-      </button>
       <button onClick={toggleTheme}>Switch to {isDarkMode ? 'light' : 'dark'} mode</button>
 
       <style jsx>{`
         header {
           display: flex;
-          justify-content: space-between;
+          justify-content: flex-end;
           align-items: center;
           padding: 1em;
           background-color: ${isDarkMode ? '#333' : '#fff'};
