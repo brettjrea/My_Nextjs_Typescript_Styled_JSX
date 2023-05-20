36be1de2cf266a0b0b4c493eab22d3a3da91c90a
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-05-20 10:31:28 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,10 +27,10 @@ export default function IndexPage() {
 return (
   <div className={isDarkMode ? 'dark' : 'light'} style={{height: '100vh', display: 'flex', flexDirection: 'column'}}>
     <div style={{ display: 'flex', flex: '1' }}>
-      <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} isDarkMode={isDarkMode}/>
       <div style={{width: '100%'}}>
         <Header isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
-        <Main />
+        <Main isDarkMode={isDarkMode}/>
       </div>
     </div>
     ...
