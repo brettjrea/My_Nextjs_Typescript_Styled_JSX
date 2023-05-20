36f3fffadff21671b8d2be0412af3d07df2c877a
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-05-20 11:16:13 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,32 @@
-// pages/index.tsx
-import { useState, useEffect } from 'react'
-
 export default function IndexPage() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-  const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
-    localStorage.setItem('isDarkMode', JSON.stringify(!isDarkMode));
-  };
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
-
-  useEffect(() => {
-    const storedThemePreference = JSON.parse(localStorage.getItem('isDarkMode'));
-    if (storedThemePreference) {
-      setIsDarkMode(storedThemePreference);
-    }
-  }, []);
-
   return (
-    <div className={isDarkMode ? 'dark' : 'light'} style={{height: '100vh', display: 'flex', flexDirection: 'column'}}>
-      <div style={{width: '100%', flex: '1'}}>
-        <main>
-          <h1>Hi there 👋</h1>
-          <h2>My name is Brett. I love computers, the internet, and learning to program for them.</h2>
-          <ul>
-            <li>🔭 I’m currently working on getting my bachelor's in computer science.</li>
-            <li>🌱 I’m currently taking Written and Verbal Communication for Software Development.</li>
-            <li>👯 I’m looking to collaborate on anything cool and relevant.</li>
-          </ul>
-          <h3>Here are some ideas to get you started:</h3>
-          <ul>
-            <li>🔭 I’m currently working on getting my bachelor's in computer science.</li>
-            <li>🌱 I’m currently learning Data Structures and Algorithms.</li>
-            <li>🤔 I’m looking for help with ...</li>
-            <li>💬 Ask me about ...</li>
-            <li>📫 How to reach me: ...</li>
-            <li>😄 Pronouns: ...</li>
-            <li>⚡ Fun fact: ...</li>
-          </ul>
-          <img src="https://github-readme-stats.vercel.app/api?username=brettjrea&show_icons=true&theme=transparent" alt="Brett J Rea's GitHub stats" width="100%" height="auto"/>
+    <main>
+      <h1>Hi there 👋</h1>
+      <h2>My name is Brett. I love computers, the internet, and learning to program for them.</h2>
+      <ul>
+        <li>🔭 I’m currently working on getting my bachelor's in computer science.</li>
+        <li>🌱 I’m currently taking Written and Verbal Communication for Software Development.</li>
+        <li>👯 I’m looking to collaborate on anything cool and relevant.</li>
+      </ul>
+      <h3>Here are some ideas to get you started:</h3>
+      <ul>
+        <li>🔭 I’m currently working on getting my bachelor's in computer science.</li>
+        <li>🌱 I’m currently learning Data Structures and Algorithms.</li>
+        <li>🤔 I’m looking for help with ...</li>
+        <li>💬 Ask me about ...</li>
+        <li>📫 How to reach me: ...</li>
+        <li>😄 Pronouns: ...</li>
+        <li>⚡ Fun fact: ...</li>
+      </ul>
+      <img src="https://github-readme-stats.vercel.app/api?username=brettjrea&show_icons=true&theme=transparent" alt="Brett J Rea's GitHub stats" width="100%" height="auto"/>
 
-          <style jsx>{`
-            main {
-              padding: 1em;
-              background: ${isDarkMode ? 'black' : 'white'};
-              color: ${isDarkMode ? 'white' : 'black'};
-            }
-          `}</style>
-        </main>
-      </div>
-    </div>
+      <style jsx>{`
+        main {
+          padding: 1em;
+          color: inherit;
+          background: inherit;
+        }
+      `}</style>
+    </main>
   )
 }
