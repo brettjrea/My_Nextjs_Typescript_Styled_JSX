a7fc322ed7649c220d35205b0b459649f573cf33
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-05-20 10:11:37 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,17 @@ import Header from '../components/Header'
 import Main from '../components/Main'
 import Sidebar from '../components/Sidebar'
 
-function MyApp({ Component, pageProps }) {
-  const [isDarkMode, setIsDarkMode] = useState(false)
-  const toggleTheme = () => setIsDarkMode(!isDarkMode)
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false); // New state
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const toggleSidebar = () => { // New function
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   return (
     <div className="app">
