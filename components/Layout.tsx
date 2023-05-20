1f8c101955fe6752ce2af4255f070600f68c9d34
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-05-20 11:28:35 -0400

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,20 +6,18 @@ import Sidebar from './Sidebar';
 type LayoutProps = {
   isDarkMode: boolean,
   toggleTheme: () => void,
-  isSidebarOpen: boolean,
-  toggleSidebar: () => void,
   children: React.ReactNode,
 };
 
-const Layout: React.FC<LayoutProps> = ({ isDarkMode, toggleTheme, isSidebarOpen, toggleSidebar, children }) => {
+const Layout: React.FC<LayoutProps> = ({ isDarkMode, toggleTheme, children }) => {
   return (
     <div className={isDarkMode ? 'dark' : 'light'} style={{height: '100vh', display: 'flex', flexDirection: 'column'}}>
+      <Header isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
       <div style={{ display: 'flex', flex: '1' }}>
-        <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} isDarkMode={isDarkMode} />
-        <div style={{width: '100%'}}>
-          <Header isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
+        <Sidebar isDarkMode={isDarkMode} />
+        <main style={{width: '100%', flex: '1', overflowY: 'auto'}}>
           {children}
-        </div>
+        </main>
       </div>
     </div>
   );
