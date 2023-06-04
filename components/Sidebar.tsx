32791dabff36228b4e46f9a21c6a467492d8f439
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-06-04 14:51:35 -0400

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,34 +1,37 @@
 // components/Sidebar.tsx
-
 import Link from 'next/link'
 
 const Sidebar = ({ isDarkMode, isSidebarOpen, toggleSidebar }) => {
   return (
     <div style={{ 
       position: 'relative', 
-      transform: `translateX(${isSidebarOpen ? '0' : '-250px'})`, 
-      transition: 'transform 0.5s ease', 
-      width: '250px', 
+      width: isSidebarOpen ? '250px' : '0px', 
       height: '100vh', 
-      backgroundColor: isDarkMode ? '#333' : '#fff' 
+      backgroundColor: isDarkMode ? '#333' : '#fff', 
+      overflow: 'hidden',
+      transition: 'width 0.5s ease',
     }}>
-      <nav style={{ width: '100%', height: '100%', overflow: 'auto' }}>
+      <nav style={{ width: '100%', height: '100%', padding: '0 20px' }}>
         {isSidebarOpen && 
           <ul>
             <li><Link href="/about">About</Link></li>
           </ul>
         }
       </nav>
-      <button onClick={toggleSidebar} style={{ position: 'absolute', right: isSidebarOpen ? '-20px' : '0', top: '0', backgroundColor: 'gray' }}>
+      <button onClick={toggleSidebar} style={{ 
+        position: 'absolute', 
+        right: isSidebarOpen ? '-20px' : '0', 
+        top: '0', 
+        backgroundColor: 'gray' 
+      }}>
         {isSidebarOpen ? '<' : '>'}
       </button>
       <style jsx>{`
-        nav {
-          transition: width 0.5s ease;
-        }
         ul {
-          padding-right: 20px;
           list-style: none;
+        }
+        li a {
+          color: ${isDarkMode ? '#fff' : '#333'};
         }
       `}</style>
     </div>
