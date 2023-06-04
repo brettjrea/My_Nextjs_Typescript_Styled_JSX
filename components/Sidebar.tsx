3ec78779974d72074023747ef052d2cf11f51ef1
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-06-04 14:53:08 -0400

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,37 +1,27 @@
 // components/Sidebar.tsx
+
 import Link from 'next/link'
 
 const Sidebar = ({ isDarkMode, isSidebarOpen, toggleSidebar }) => {
   return (
-    <div style={{ 
-      position: 'relative', 
-      width: isSidebarOpen ? '250px' : '0px', 
-      height: '100vh', 
-      backgroundColor: isDarkMode ? '#333' : '#fff', 
-      overflow: 'hidden',
-      transition: 'width 0.5s ease',
-    }}>
-      <nav style={{ width: '100%', height: '100%', padding: '0 20px' }}>
+    <div style={{ position: 'relative', width: isSidebarOpen ? '250px' : '0', transition: 'width 0.5s ease', height: '100vh' }}>
+      <nav style={{ width: '100%', height: '100%', overflow: 'auto' }}>
         {isSidebarOpen && 
           <ul>
             <li><Link href="/about">About</Link></li>
           </ul>
         }
       </nav>
-      <button onClick={toggleSidebar} style={{ 
-        position: 'absolute', 
-        right: isSidebarOpen ? '-20px' : '0', 
-        top: '0', 
-        backgroundColor: 'gray' 
-      }}>
+      <button onClick={toggleSidebar} style={{ position: 'absolute', right: '-20px', top: '0', backgroundColor: 'gray' }}>
         {isSidebarOpen ? '<' : '>'}
       </button>
       <style jsx>{`
-        ul {
-          list-style: none;
+        nav {
+          transition: width 0.5s ease;
         }
-        li a {
-          color: ${isDarkMode ? '#fff' : '#333'};
+        ul {
+          padding-right: 20px;
+          list-style: none;
         }
       `}</style>
     </div>
