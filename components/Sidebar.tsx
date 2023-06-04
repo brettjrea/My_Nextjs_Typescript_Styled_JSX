60a9025917018eb612e6bca3c011127c3a296a7e
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-06-04 14:10:52 -0400

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,11 +3,13 @@ import Link from 'next/link'
 
 const Sidebar = ({ isDarkMode, isSidebarOpen, toggleSidebar }) => {
   return (
-    <div style={{ position: 'relative', width: isSidebarOpen ? '250px' : '0px', height: '100vh' }}>
-      <nav style={{ visibility: isSidebarOpen ? 'visible' : 'hidden', width: '250px' }}>
-        <ul>
-          <li><Link href="/about">About</Link></li>
-        </ul>
+    <div style={{ position: 'relative', width: isSidebarOpen ? '250px' : '0', transition: 'width 0.5s ease', height: '100vh' }}>
+      <nav>
+        {isSidebarOpen && 
+          <ul>
+            <li><Link href="/about">About</Link></li>
+          </ul>
+        }
       </nav>
       <button onClick={toggleSidebar} style={{ position: 'absolute', right: '-20px', top: '0', backgroundColor: 'gray' }}>
         {isSidebarOpen ? '<' : '>'}
@@ -16,7 +18,6 @@ const Sidebar = ({ isDarkMode, isSidebarOpen, toggleSidebar }) => {
         nav {
           background-color: #f5f5f5;
           overflow: auto;
-          transition: visibility 0.5s ease;
         }
         ul {
           padding-right: 20px;
