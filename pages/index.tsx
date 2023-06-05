c3bf3ee246adcceb679fcb0304e1b8c0d126ad4f
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-06-04 21:48:06 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function IndexPage() {
         <li>⚡ Fun fact: ...</li>
       </ul>
       <img src="https://my-github-readme-stats-steel.vercel.app//api?username=brettjrea&rank_icon=github&theme=transparent" alt="Brett J Rea's GitHub stats" width="100%" height="auto"/>
-      <img src="https://my-github-readme-stats-steel.vercel.app/api/top-langs/?username=brettjrea)" alt="Brett J Rea's GitHub stats" width="100%" height="auto"/>
+      <img src="https://my-github-readme-stats-steel.vercel.app/api/top-langs/?username=brettjrea" alt="Brett J Rea's GitHub stats" width="100%" height="auto"/>
       <style jsx>{`
         main {
           padding: 1em;
