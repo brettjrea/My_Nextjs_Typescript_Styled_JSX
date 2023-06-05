616a85f0a88c7da79b4903c223e7ce42d5c4f0f4
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-06-05 15:23:28 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,9 @@
 const Header = ({ isDarkMode, toggleTheme }) => {
   return (
     <header>
-      <button onClick={toggleTheme}>Switch to {isDarkMode ? 'light' : 'dark'} mode</button>
+      <button className={isDarkMode ? 'dark-mode' : ''} onClick={toggleTheme}>
+        Switch to {isDarkMode ? 'light' : 'dark'} mode
+      </button>
 
       <style jsx>{`
         header {
@@ -13,10 +15,26 @@ const Header = ({ isDarkMode, toggleTheme }) => {
           background-color: ${isDarkMode ? '#333' : '#fff'};
           color: ${isDarkMode ? '#fff' : '#333'};
         }
+
+        button {
+          padding: 10px 20px;
+          border-radius: 5px;
+          border: none;
+          background-color: #888;
+          box-shadow: 3px 3px 5px rgba(0,0,0,0.3);
+          color: white;
+          font-size: 1rem;
+          transition: all 0.3s ease;
+        }
+
+        button.dark-mode {
+          background-color: #ccc;
+          box-shadow: inset 1px 1px 2px rgba(0,0,0,0.3);
+          color: black;
+        }
       `}</style>
     </header>
   )
 }
 
 export default Header;
-
