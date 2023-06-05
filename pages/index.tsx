1186609f5bcc1ac3b5879ec7b9d24b2850adf547
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-06-05 19:02:31 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,8 @@ function IndexPage() {
       <h2>My name is Brett. I love computers, the internet, and learning to program for them.</h2>
       <ul>
         <li>🔭 I’m currently working on getting my bachelor's in computer science.</li>
-        <li>🌱 I’m currently taking Written and Verbal Communication for Software Development.</li>
+        <li>🌱 I’m currently taking Java III.</li>
         <li>👯 I’m looking to collaborate on anything cool and relevant.</li>
-      </ul>
-      <h3>Here are some ideas to get you started:</h3>
-      <ul>
-        <li>🔭 I’m currently working on getting my bachelor's in computer science.</li>
-        <li>🌱 I’m currently learning Data Structures and Algorithms.</li>
         <li>🤔 I’m looking for help with ...</li>
         <li>💬 Ask me about ...</li>
         <li>📫 How to reach me: ...</li>
