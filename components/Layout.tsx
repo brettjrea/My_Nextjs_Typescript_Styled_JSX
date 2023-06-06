5f93199674ddb4d2a7bc55a61f078738fda11a7e
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-06-05 23:07:41 -0400

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -25,6 +25,9 @@ const Layout: React.FC<LayoutProps> = ({ isDarkMode, toggleTheme, isSidebarOpen,
         div {
           background-color: ${isDarkMode ? '#333' : '#fff'};
           color: ${isDarkMode ? '#fff' : '#333'};
+          text-shadow: ${isDarkMode ? '-1px -1px 1px rgba(0,0,0,0.3), 1px 1px 1px rgba(255,255,255,0.5)' : '2px 2px 2px rgba(0,0,0,0.5)'};
+      }
+      }
         }
       `}</style>
     </div>
